b77556552fb16f7cb485ccf3ca052503502b3339
Cherry Mirra Calisnao <99376788+mirracalisnao@users.noreply.github.com>
2024-02-29 18:22:42 +0800

    Edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def display_form2():
     X = df.iloc[:,:-1].values
     y = df.iloc[:,-1].values 
 
-    form2.text(df) 
+    form2.write(df) 
     # insert the rest of the code to train the classifier here        
     form2.write('Display the training result')
 
